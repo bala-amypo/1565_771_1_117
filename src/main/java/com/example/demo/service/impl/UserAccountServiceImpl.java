6aa02ef81d0b147923a27c5dfdfcbf60078f03e2
bala-amypo <bala@amypo.in>
2025-12-16 16:08:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -15,6 +15,27 @@ public class UserAccountServiceImpl implements UserAccountService{
         return users;
     }
     public UserAccount createUser(UserAccount user){
-        user
+        user.setId((long) id++);
+        users.add(user);
+        return user;
+    }
+    public UserAccount getUserById(Long id){
+        for(UserAccount u: users){
+            if(u.getId().equals(id)){
+                return u;
+            }
+        }
+        return null;
+    }
+    public UserAccount findByUsername(String username){
+        for(UserAccount u: users){
+            if(u.getUsername().equals(username)){
+                return u;
+            }
+        }
+        return null;
+    }
+    public UserAccount updateUser(Long id,UserAccount user){
+        for()
     }
 }
