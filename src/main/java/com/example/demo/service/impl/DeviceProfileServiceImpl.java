794fe6d449b68aacce9c6a1a624cb92ad26e7f15
bala-amypo <bala@amypo.in>
2025-12-16 16:48:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
@@ -5,4 +5,21 @@
 
 
 @Service
-public class DeviceProfileServiceImpl
+public class DeviceProfileServiceImpl implements DeviceProfileService {
+    List<DeviceProfile> devices=new ArrayList<>();
+    long id;
+
+    public DeviceProfile registerDevice(DeviceProfile device){
+        device.setId((long)id++);
+        devices.add(device);
+        return device;
+    }
+    public DeviceProfile updateTrustStatus(Long id, boolean trust){
+        for(DeviceProfile d : devices){
+            if(d.getId().equals(id)){
+                d.setIsTrusted(trust);
+                
+            }
+        }
+    }
+}
