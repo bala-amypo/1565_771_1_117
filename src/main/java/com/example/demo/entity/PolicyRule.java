c966006c1c6f3cfc9f775b3a443a36a615f609e6
bala-amypo <bala@amypo.in>
2025-12-16 10:13:46 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/PolicyRule.java
@@ -0,0 +1,61 @@
+package com.example.demo1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class PolicyRule {
+    @Id
+    private long id;
+    private String ruleCode;
+    private String description;
+    private String severity;
+    private String conditionsJson;
+    private Boolean active;
+    public PolicyRule() {
+    }
+    public PolicyRule(String ruleCode, String description, String severity, String conditionsJson, Boolean active) {
+        this.ruleCode = ruleCode;
+        this.description = description;
+        this.severity = severity;
+        this.conditionsJson = conditionsJson;
+        this.active = active;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getRuleCode() {
+        return ruleCode;
+    }
+    public void setRuleCode(String ruleCode) {
+        this.ruleCode = ruleCode;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getSeverity() {
+        return severity;
+    }
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+    public String getConditionsJson() {
+        return conditionsJson;
+    }
+    public void setConditionsJson(String conditionsJson) {
+        this.conditionsJson = conditionsJson;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    
+}
