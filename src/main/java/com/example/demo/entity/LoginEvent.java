da56dbff1b148200855894bff10dec7877aad903
bala-amypo <bala@amypo.in>
2025-12-16 10:16:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/LoginEvent.java
@@ -0,0 +1,72 @@
+package com.example.demo1.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class LoginEvent {
+    @Id
+    private long id;
+    private long userId;
+    private String ipAddress;
+    private String location;
+    private String deviceId;
+    private LocalDateTime timestamp;
+    private String loginStatus;
+    public LoginEvent() {
+    }
+    public LoginEvent(long id, long userId, String ipAddress, String location, String deviceId, LocalDateTime timestamp,
+            String loginStatus) {
+        this.id = id;
+        this.userId = userId;
+        this.ipAddress = ipAddress;
+        this.location = location;
+        this.deviceId = deviceId;
+        this.timestamp = timestamp;
+        this.loginStatus = loginStatus;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public long getUserId() {
+        return userId;
+    }
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+    public String getIpAddress() {
+        return ipAddress;
+    }
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getDeviceId() {
+        return deviceId;
+    }
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+}
