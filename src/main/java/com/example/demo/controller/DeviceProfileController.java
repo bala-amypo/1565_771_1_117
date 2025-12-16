337a75b47cc0413cc07210ec9f8fbd95df7fe5c5
bala-amypo <bala@amypo.in>
2025-12-16 17:48:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/DeviceProfileController.java
+++ src/main/java/com/example/demo/controller/DeviceProfileController.java
@@ -8,4 +8,12 @@
 
 
 
-@Resy
+@RestController
+@RequestMapping("/auth")
+public class AuthController{
+    @Autowired
+    UserAccountService userAccountService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserAccount> register(@)
+}
