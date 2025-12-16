9453b7ebafeb635234d03520fc56120fb176a90c
bala-amypo <bala@amypo.in>
2025-12-16 17:18:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ViolationRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ViolationRecordServiceImpl.java
@@ -3,3 +3,16 @@
 
 
 
+@Service
+public class ViolationRecordServiceImpl implements ViolationRecordService{
+    List<ViolationRecord> violations = new ArrayList<>();
+    long id=1;
+    public ViolationRecord logViolation(ViolationRecord violation){
+        violation.setId((long) id++);
+        violations.add(violation);;
+        return violation;
+    }
+    public List<ViolationRecord> getViolationsByUser(Long userId){
+        List<ViolationRecord> result =
+    }
+}
