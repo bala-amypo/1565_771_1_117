460bff5f61a0c9bf4dfabfdf9e52ac4a754a8eec
bala-amypo <bala@amypo.in>
2025-12-16 14:13:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoginEvent.java
+++ src/main/java/com/example/demo/entity/LoginEvent.java
@@ -1,4 +1,4 @@
-package com.example.demo1.model;
+package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
--- src/main/java/com/example/demo/entity/PolicyRule.java
+++ src/main/java/com/example/demo/entity/PolicyRule.java
@@ -1,4 +1,4 @@
-package com.example.demo1.model;
+package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
