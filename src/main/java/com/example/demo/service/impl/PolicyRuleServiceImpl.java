9a621e48ae543a9a241637464591b95c0012a49d
bala-amypo <bala@amypo.in>
2025-12-16 17:03:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
@@ -5,6 +5,20 @@
 public class PolicyRuleServiceImpl implements PolicyRuleService {
     List<PolicyRule> rules = new ArrayList<>();
     long id=1;
-    public 
+    public PolicyRule createRule(PolicyRule rule){
+        rule.setId((long) id++);
+        rules.add(rule);
+        return rule;
+    }
+    public PolicyRule updateRule(Long id,PolicyRule rule){
+        for(PolicyRule r: rules){
+            if(r.getId().equals(id)){
+                r.setRuleCode(rule.getRuleCode());
+                r.setDescripton(rule.getDescription());
+                r.setSeverity(rule.getSeverity());
+                r.setConditionsJson(rule.getConditions)
+            }
+        }
+    }
 
 }
