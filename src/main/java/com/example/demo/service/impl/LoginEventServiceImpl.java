ef7946f25ad1bedf358acf01d3774ddb13314870
bala-amypo <bala@amypo.in>
2025-12-16 16:33:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LoginEventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoginEventServiceImpl.java
@@ -6,5 +6,16 @@
 
 @Service
 public class LoginEventServiceImpl implements LoginEventService{
-    List<LoginEvent> events =new 
+    List<LoginEvent> events =new ArrayList<>();
+    long id=1;
+    public void recordLogin(LoginEvent event){
+        event.setId((long) id++);
+        events.add(event);
+    }
+    public List<LoginEvent> getEventByUser(Long userId){
+        List<LoginEvent> result =new ArrayList<>();
+        for(LoginEvent e: events){
+            if(e)
+        }
+    }
 }
