5adec16f01dd0b428570ec23e75c2ee57d1fe0f7
bala-amypo <bala@amypo.in>
2025-12-17 05:18:46 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LoginEventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoginEventServiceImpl.java
@@ -7,30 +7,31 @@
 @Service
 public class LoginEventServiceImpl implements LoginEventService{
     List<LoginEvent> events =new ArrayList<>();
-    long id=1;
-    public void recordLogin(LoginEvent event){
-        event.setId((long) id++);
-        events.add(event);
-    }
-    public List<LoginEvent> getEventByUser(Long userId){
-        List<LoginEvent> result =new ArrayList<>();
-        for(LoginEvent e: events){
-            if(e.getUserId().equals(userId)){
-                result.add(e);
-            }
-        }
-        return result;
-    }
-    public List<LoginEvent> getSuspiciousLogins(Long userId){
-        List<LoginEvent> result = new ArrayList<>();
-        for(LoginEvent e : events){
-            if(e.getUserId().equals(userId) && "Falied".euqla(e.getLoginStatus())){
-                result.add(e);
-            }
-        }
-        return result;
-    }
-    public List<LoginEvent> getAllEvents(){
-        return events;
-    }
-}
+        long id=1;
+            public void recordLogin(LoginEvent event){
+                    event.setId((long) id++);
+                            events.add(event);
+                                }
+                                    public List<LoginEvent> getEventByUser(Long userId){
+                                            List<LoginEvent> result =new ArrayList<>();
+                                                    for(LoginEvent e: events){
+                                                                if(e.getUserId().equals(userId)){
+                                                                                result.add(e);
+                                                                                            }
+                                                                                                    }
+                                                                                                            return result;
+                                                                                                                }
+                                                                                                                    public List<LoginEvent> getSuspiciousLogins(Long userId){
+                                                                                                                            List<LoginEvent> result = new ArrayList<>();
+                                                                                                                                    for(LoginEvent e : events){
+                                                                                                                                                if(e.getUserId().equals(userId) && "Falied".euqla(e.getLoginStatus())){
+                                                                                                                                                                result.add(e);
+                                                                                                                                                                            }
+                                                                                                                                                                                    }
+                                                                                                                                                                                            return result;
+                                                                                                                                                                                                }
+                                                                                                                                                                                                    public List<LoginEvent> getAllEvents(){
+                                                                                                                                                                                                            return events;
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                                
