ad190a6b8f9023bb17ab16eb01c58d5fee9d0b07
bala-amypo <bala@amypo.in>
2025-12-18 05:37:52 +0000

    User commit

--- src/main/java/com/example/demo/service/ViolationRecordService.java
+++ src/main/java/com/example/demo/service/ViolationRecordService.java
@@ -25,6 +25,6 @@ public interface ViolationRecordService{
     ViolationRecord logViolation(ViolationRecord violation);
     List<ViolationRecord> getViolationsByUser(Long userId);
     ViolationRecord markResolved(Long id);
-    List<ViolationRecord> getUnResolvedViolations();
     List<ViolationRecord> getAllViolations();
+    List<ViolationRecord> getUnresolvedViolations();
 }
