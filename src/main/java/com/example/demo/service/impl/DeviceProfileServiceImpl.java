ba58cf5ed38352f9b9be70f90b1f06ed8968f236
bala-amypo <bala@amypo.in>
2025-12-18 04:52:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.impl;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,35 +11,45 @@ import com.example.demo.service.DeviceProfileService;
 
 @Service
 public class DeviceProfileServiceImpl implements DeviceProfileService {
-    List<DeviceProfile> devices=new ArrayList<>();
-    long id;
 
-    public DeviceProfile registerDevice(DeviceProfile device){
+    List<DeviceProfile> devices = new ArrayList<>();
+    long id = 1;
+
+    @Override
+    public DeviceProfile registerDevice(DeviceProfile device) {
         device.setId(id++);
+        device.setLastSeen(LocalDateTime.now());
+        device.setIsTrusted(false);
         devices.add(device);
         return device;
     }
-    public DeviceProfile updateTrustStatus(Long id, boolean trust){
-        for(DeviceProfile d : devices){
-            if(d.getId()==id){
+
+    @Override
+    public DeviceProfile updateTrustStatus(Long id, boolean trust) {
+        for (DeviceProfile d : devices) {
+            if (d.getId()==(id)) {
                 d.setIsTrusted(trust);
                 return d;
             }
         }
         return null;
     }
-    public List<DeviceProfile> getDeviceByUser(Long userId){
+
+    @Override
+    public List<DeviceProfile> getDevicesByUser(Long userId) {
         List<DeviceProfile> result = new ArrayList<>();
-        for(DeviceProfile d : devices){
-            if(d.getUserId()==userId){
+        for (DeviceProfile d : devices) {
+            if (d.getUserId()==(userId)) {
                 result.add(d);
             }
         }
         return result;
     }
-    public DeviceProfile findByDeviceId(String deviceId){
-        for(DeviceProfile d: devices){
-            if(d.getDeviceId().equals(deviceId)){
+
+    @Override
+    public DeviceProfile findByDeviceId(String deviceId) {
+        for (DeviceProfile d : devices) {
+            if (d.getDeviceId()==(deviceId)) {
                 return d;
             }
         }
