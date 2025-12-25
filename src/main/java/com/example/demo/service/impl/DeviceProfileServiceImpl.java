bb8448497b96f0cf259dad6811ca0b6541779d79
bala-amypo <bala@amypo.in>
2025-12-25 14:13:25 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviceProfileServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service   // ✅ THIS WAS MISSING
+@Service  
 public class DeviceProfileServiceImpl implements DeviceProfileService {
 
     private DeviceProfileRepository deviceRepo;
