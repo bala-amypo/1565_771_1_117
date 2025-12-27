3f3102d129198a1dabd62008b423e38ab42c074b
bala-amypo <bala@amypo.in>
2025-12-27 14:55:45 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,14 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,25 +13,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("IT Policy Management API")
-                        .version("1.0")
-                        .description("Swagger documentation for IT Policy Violation System"))
-                
-                // 🔹 SERVER CONFIGURATION
-                .servers(List.of(
-                        new Server()
-                                .url("https://9089.pro604cr.amypo.ai")
-                                .description("Local Development Server")
-                ))
+        final String securitySchemeName = "bearerAuth";
 
-                // 🔐 JWT SECURITY
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
-                        new io.swagger.v3.oas.models.Components()
-                                .addSecuritySchemes("BearerAuth",
-                                        new SecurityScheme()
+                new Components()
+                .addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                                .name("Authorization")
                                                 .type(SecurityScheme.Type.HTTP)
                                                 .scheme("bearer")
                                                 .bearerFormat("JWT")
