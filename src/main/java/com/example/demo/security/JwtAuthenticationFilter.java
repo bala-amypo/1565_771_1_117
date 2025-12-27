9124a4e19d5e5730f079b275c1f67863cf488347
bala-amypo <bala@amypo.in>
2025-12-27 05:48:07 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.filter.OncePerRequestFilter;
-
+import com.example.demo.security.JwtUtil;
 import java.io.IOException;
 import java.util.Collections;
 
