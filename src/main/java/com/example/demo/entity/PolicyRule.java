16a3756b6c99fe4639ccdcd1f4ed24df3f326707
bala-amypo <bala@amypo.in>
2025-12-28 14:34:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/PolicyRule.java
+++ src/main/java/com/example/demo/entity/PolicyRule.java
@@ -31,7 +31,7 @@ public class PolicyRule {
 
     // Getters and Setters
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id}
+    public void setId(Long id) { this.id = id; }
     public String getRuleCode() { return ruleCode; }
     public void setRuleCode(String ruleCode) { this.ruleCode = ruleCode; }
     public String getDescription() { return description; }
