c239aeb70b1dd063c8e81e4878beb6274b47fc29
bala-amypo <bala@amypo.in>
2025-12-28 15:43:08 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
@@ -46,7 +46,7 @@
 //         return repo.findByRuleCode(ruleCode).orElse(null);
 //     }
 //}
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.PolicyRule;
 import com.example.demo.repository.PolicyRuleRepository;
@@ -58,43 +58,32 @@ import java.util.List;
 @Service
 public class PolicyRuleServiceImpl implements PolicyRuleService {
 
-    private final PolicyRuleRepository ruleRepo;
+    private final PolicyRuleRepository repo;
 
-    public PolicyRuleServiceImpl(PolicyRuleRepository ruleRepo) {
-        this.ruleRepo = ruleRepo;
+    public PolicyRuleServiceImpl(PolicyRuleRepository repo) {
+        this.repo = repo;
     }
 
-    @Override
     public PolicyRule createRule(PolicyRule rule) {
-        return ruleRepo.save(rule);
+        return repo.save(rule);
     }
 
-    @Override
     public PolicyRule updateRule(Long id, PolicyRule rule) {
-        PolicyRule existing = ruleRepo.findById(id)
-                .orElseThrow(() -> new RuntimeException("Rule not found with ID: " + id));
-
-        existing.setDescription(rule.getDescription());
-        existing.setSeverity(rule.getSeverity());
-        existing.setConditionsJson(rule.getConditionsJson());
-        existing.setActive(rule.getActive());
-
-        return ruleRepo.save(existing);
+        rule.setId(id);
+        return repo.save(rule);
     }
 
-    @Override
-    public PolicyRule getRuleByCode(String code) {
-        return ruleRepo.findByRuleCode(code)
-                .orElseThrow(() -> new RuntimeException("Rule not found with code: " + code));
-    }
-
-    @Override
     public List<PolicyRule> getActiveRules() {
-        return ruleRepo.findByActiveTrue();
+        return repo.findByActiveTrue();
     }
 
-    @Override
     public List<PolicyRule> getAllRules() {
-        return ruleRepo.findAll();
+        return repo.findAll();
     }
+
+    public PolicyRule getRuleByCode(String ruleCode){
+    return repo.findByRuleCode(ruleCode);
+    }
+
 }
+
