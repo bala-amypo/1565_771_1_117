301581b6fe82a6cb0c6c5718da4050efd48db183
bala-amypo <bala@amypo.in>
2025-12-28 15:31:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/PolicyRuleRepository.java
+++ src/main/java/com/example/demo/repository/PolicyRuleRepository.java
@@ -1,28 +1,28 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.entity.PolicyRule;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import java.util.List;
-// import java.util.Optional;
-
-// public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
-
-//     List<PolicyRule> findByActiveTrue();
-
-//     Optional<PolicyRule> findByRuleCode(String ruleCode);
-// }
-
-//}
 package com.example.demo.repository;
 
 import com.example.demo.entity.PolicyRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
 
-  
+    List<PolicyRule> findByActiveTrue();
+
     Optional<PolicyRule> findByRuleCode(String ruleCode);
 }
+
+}
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.PolicyRule;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.util.Optional;
+
+// public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
+
+  
+//     Optional<PolicyRule> findByRuleCode(String ruleCode);
+// }
